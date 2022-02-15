d7af7469ea85e93204f078c05450ea93eaa80971
Kowall, Marian (SE-A/31) <marian.kowall@volkswagen.de>
2022-02-15 15:07:57 +0100

    check

--- React/src/App.test.js
+++ React/src/App.test.js
@@ -1,9 +1,59 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Enzyme, { mount } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+
+
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe('isValidCarTest ', () => {
+
+
+  it('check isValidCar function  with x year and xxxxx year', () => {
+    const wrapper =  mount(<App />);
+    
+    let carIsValidCarWithOneYear = {make: 'mazda', year:'1'};
+
+    let carIsValidCarWith5SignYear = {make: 'mazda', year:'19911'};
+    const expectedTrue = wrapper.instance().isValidCar(carIsValidCarWithOneYear);
+    const expectedFalse = wrapper.instance().isValidCar(carIsValidCarWith5SignYear);
+    expect(expectedTrue).toBe(false);
+    expect(expectedFalse).toBe(false);
+  })
+
+
+  it('check isValidCar function (mazda year: xxxx min) ', () => {
+    const wrapper =  mount(<App />);
+    
+    let carValidTest = {make: 'mazda', year:'97'};
+    let carInValidTest = {make: 'mazda', year:'1996'};
+    const expectedTrue = wrapper.instance().isValidCar(carValidTest);
+    const expectedFalse = wrapper.instance().isValidCar(carInValidTest);
+    expect(expectedTrue).toBe(true);
+    expect(expectedFalse).toBe(false);
+  })
+
+  // it('check isValidCar function (mazda year: xxxx max) ', () => {
+  //   const wrapper =  mount(<App />);
+    
+  //   let carValidTest = {make: 'mazda', year:'2006'};
+  //   let carInValidTest = {make: 'mazda', year:'2007'};
+  //   const expectedTrue = wrapper.instance().isValidCar(carValidTest);
+  //   const expectedFalse = wrapper.instance().isValidCar(carInValidTest);
+  //   expect(expectedTrue).toBe(true);
+  //   expect(expectedFalse).toBe(false);
+  // })
+
+
+})
+
